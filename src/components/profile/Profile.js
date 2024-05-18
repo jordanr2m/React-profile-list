a7a16990da01927f80c540e3eab5e3ebb2cdae51
Jordan J <jordanr2m@gmail.com>
2024-05-18 13:38:29 -0700

    Finished adding styles to app.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,20 +6,27 @@ import { FaTrashAlt } from "react-icons/fa";
 // In order to map over profileData, we must put it in a state. Now, in order to access the array, we will use userProfile
 const Profile = () => {
   const [userProfile, setUserProfile] = useState(profileData);
+
+  const handleDelete = () => {
+    userProfile.filter((profile) => {
+        
+    })
+  }
     
   return (
     <section className='profile-sec'>
       <div className='container'>
-        <h2>User Profiles</h2>
+        <h2 className="--text-light">User Profiles</h2>
         {userProfile.map((profile) => (
-            <div className="profile" key={profile.id}>
+            <div className="profile --card --flex-between" key={profile.id}>
                 <img src={profile.img} alt="profile" />
                 <div className="description">
-                    <h4>Name: {profile.name}</h4>
-                    <p>Job: {profile.job}</p>
+                    <h4 className="--text-light">Name: {profile.name}</h4>
+                    <p className="--text-light">Job: {profile.job}</p>
                 </div>
-                <FaTrashAlt 
+                <FaTrashAlt className="icon"
                     size={18}
+                    // color={"white"}
                 />
             </div>
         ))}
